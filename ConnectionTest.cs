a4d32355f137a9c97d7cea9dc6eb4c0662ca47b2
Chris <Christopher.kyame@laverne.edu>
2020-02-11 23:59:55 -0800

    init

---
+++ ConnectionTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Tests.SQL;
+
+namespace Tests
+{
+    [TestClass]
+    public class ConnectionTest
+    {
+        string connectionString = "Data Source=TEST123\\SQLEXPRESS;Initial Catalog=Northwind;Trusted_Connection=true;";
+
+        [TestMethod]
+        public void CanConnectToDatabase()
+        {
+            ConnectToDatabase();
+        }
+
+        [TestMethod]
+        public void CanUseSQLCQ()
+        {
+            SQLCQ sqlcq = new SQLCQ(connectionString);
+            List<NorthWindObject> d = sqlcq.ExecuteQuery<NorthWindObject>("SELECT * FROM [Categories]");
+            var x = 1;
+        }
+
+        [TestMethod]
+        public void CanUseParamsWithSQLCQ()
+        {
+            SQLCQ sqlcq = new SQLCQ(connectionString);         
+            List<NorthWindObject> d = sqlcq.ExecuteQuery<NorthWindObject>(
+                "SELECT * FROM [Categories] WHERE CategoryID = @catId", new[] {
+                    new SQLCQParameter() {
+                        Name = "@catId", 
+                        value = 2
+                    }
+                });
+            var x = 1;
+        }
+
+        private void ConnectToDatabase()
+        {
+
+            string queryString = "SELECT ProductID, UnitPrice, ProductName from dbo.products WHERE UnitPrice > @pricePoint ORDER BY UnitPrice DESC;";
+            int paramValue = 5;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.AddWithValue("@pricePoint", paramValue);
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        NorthWindObject o = new NorthWindObject();
+                        string n = o.GetType().GetFields()[0].Name;
+                        Console.WriteLine("\t{0}\t{1}\t{2}", reader[0], reader[1], reader[2]);
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+
+    public class NorthWindObject
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+
+        public NorthWindObject()
+        {
+            CategoryName = string.Empty;
+            CategoryID = default(int);
+        }
+    }
+}
---
+++ SQL/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace Tests.SQL
+{
+    public class SQLCQParameter
+    {
+        public string Name { get; set; }
+        public object value { get; set; }
+    }
+
+    public class SQLCQ
+    {
+        private string ConnectionString { get; set; }
+
+        public SQLCQ(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public List<T> ExecuteQuery<T>(string query, SQLCQParameter[] param = null)
+        {
+            List<T> tl = Activator.CreateInstance<List<T>>();
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                if (param != null && param.Length != 0)
+                    for (int i = 0; i < param.Length; i++)
+                        command.Parameters.AddWithValue(param[i].Name, param[i].value);
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        int totalColumns = reader.FieldCount;
+                        T t = Activator.CreateInstance<T>();
+                        for (int i = 0; i < totalColumns; i++)
+                            t.GetType()
+                                .GetProperty(reader.GetName(i))?
+                                .SetValue(t, reader.GetValue(i));
+                        tl.Add(t);
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return tl;
+        }
+    }
+}
